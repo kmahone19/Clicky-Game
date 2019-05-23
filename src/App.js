c767cf2694110a5ca4376b09567b2c354a6749da
Kevin M <kevin.mahone19@gmail.com>
2019-05-23 16:46:59 -0400

    added additional checks

--- src/App.js
+++ src/App.js
@@ -4,31 +4,49 @@ import ImageCard from './components/imageCard/imageCard';
 
 const imageArr = [
   {
-
+    id: 1,
+    image: "",
+    name: ""
   },
   {
-
+    id: 2,
+    image: "",
+    name: ""
   },
   {
-
+    id: 3,
+    image: "",
+    name: ""
   },
   {
-
+    id: 4,
+    image: "",
+    name: ""
   },
   {
-
+    id: 5,
+    image: "",
+    name: ""
   },
   {
-
+    id: 6,
+    image: "",
+    name: ""
   },
   {
-
+    id: 7,
+    image: "",
+    name: ""
   },
   {
-
+    id: 8,
+    image: "",
+    name: ""
   },
   {
-
+    id: 9,
+    image: "",
+    name: ""
   }
 ]
 
@@ -41,8 +59,15 @@ class App extends React.Component {
   }
 
   handleIamgeClick = () => {
-    this.setState({ count: this.state.count + 1 })
-    
+
+    if (this.state.imgList.clicked) {
+      this.setState({ count: this.state.count + 1 });
+      this.handleHighScores();
+      this.shuffleCards();
+    } else if (!this.state.imgList.clicked) {
+      this.setState({ count: 0 });
+      this.shuffleCards();
+    }
   };
 
   shuffleCards = () => {
@@ -53,18 +78,24 @@ class App extends React.Component {
     });
   };
 
+  handleHighScores = () => {
+    if (this.state.count === this.state.high) {
+      this.setState({ high: this.state.count })
+    }
+  }
+
   render() {
     return (
       <div>
         <Header count={this.state.count} high={this.state.high} />
         <div className="container">
-        {
-          this.state.imgList.map(image => {
-            return (
-              <ImageCard image={image.image} handleIamgeClick={this.handleIamgeClick} key={image.id} name={image.name} />
-            )
-          })
-        }
+          {
+            this.state.imgList.map(image => {
+              return (
+                <ImageCard image={image.image} handleIamgeClick={this.handleIamgeClick} key={image.id} name={image.name} />
+              )
+            })
+          }
         </div>
       </div>
     )
