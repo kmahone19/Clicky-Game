8d148199899e5ff9fdc45e8fc819167e42a2995d
Kevin M <kevin.mahone19@gmail.com>
2019-05-29 12:05:50 -0400

    created build

---
+++ build/precache-manifest.5976d01fb33a8400ab48c9e33fd1a8ad.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e64c1b12db5dbb3aa757f5f61aeb39eb",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/Clicky-Game/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "68f115eed0cd0853892f",
+    "url": "/Clicky-Game/static/js/main.fd68c4ce.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  }
+]);
